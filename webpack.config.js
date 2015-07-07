ca7c3ae0619440ddaeedd1c03930767a648f96f1
Param Aggarwal <paramaggarwal@gmail.com>
2015-07-07 14:26:49 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ var webpack = require('webpack');
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
       },
     ],
   },
